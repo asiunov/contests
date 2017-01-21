f2aab6fc322b3f11c2c1744a5aa728be0c1aeb08
Andrey Siunov <asiunov@zenefits.com>
2017-01-21 14:52:23 -0800

    fixed bug in FacebookHackerCup2017-A

--- src/FacebookHackerCup2017/SolutionA.java
+++ src/FacebookHackerCup2017/SolutionA.java
@@ -23,7 +23,9 @@ public class SolutionA {
 
     private int f1(int n, int m, int k) {
         if (n - 2 >= 2 * k + 1) {
-            return m / k + (m % k == 0 ? 0 : 1);
+            if (m > k) {
+                return m / k + (m % k == 0 ? 0 : 1);
+            }
         }
         return INF;
     }
